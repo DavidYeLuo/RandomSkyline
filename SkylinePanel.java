fddaa70d1a50f1ddd20f3e790c1247ff6c16537f
Kenta Medina <kenta.medina@gmail.com>
2016-10-06 21:32:38 -0700

    Add files via upload

--- SkylinePanel.java
+++ SkylinePanel.java
@@ -11,8 +11,8 @@ import java.lang.*;
 
 public class SkylinePanel extends JPanel
 {
-   private Building building1;
-   private Star shinyThing;
+    private Building building1; //instantiate
+    private Star shinyThing;
    //-------------------------------------------------
    //   Default Constructor: Sets up the Panel
    //-------------------------------------------------
@@ -26,6 +26,7 @@ public class SkylinePanel extends JPanel
    //-------------------------------------------------
    public void paintComponent (Graphics page)
    {
+
       super.paintComponent(page);
       for(int i = 0; i < 100; i++){
         shinyThing = new Star((int)(Math.random()*600), (int)(Math.random()*380), Color.black, 5, 5);
@@ -35,30 +36,20 @@ public class SkylinePanel extends JPanel
       page.setColor(Color.black);  // moon
       page.fillOval(50,70,40,40);      // moon
       
-      // page.setColor(new Color(10,10,10));   // far background building
-      // page.fillRect(0,340,600,60);          // far background building
-      // for(int i = 0; i < 40; i++){
-      //    page.fillRect((int)(Math.random()*680-40)  // far background building
-      //                 ,(int)(Math.random()*40+305)  // far background building
-      //                 ,(int)(Math.random()*20+20)   // far background building
-      //                 ,40);                         // far background building
-      // }
-      
-      // page.setColor(new Color(7,7,7));      // close background building
-      // page.fillRect(0,360,600,20);          // close background building
-      // for(int i = 0; i < 40; i++){
-      //    page.fillRect((int)(Math.random()*680-40)  // close background building
-      //                 ,(int)(Math.random()*20+345)  // close background building
-      //                 ,(int)(Math.random()*20+20)   // close background building
-      //                 ,60);                         // close background building
-      // }
+      page.setColor(new Color(0,0,0));   // far background building
+      for(int i = 0; i < 40; i++){
+         page.fillRect((int)(Math.random()*680-40)  // far background building
+                      ,(int)(Math.random()*20+360)  // far background building
+                      ,(int)(Math.random()*20+40)   // far background building
+                      ,40);                         // far background building
+      }
       
       page.setColor(new Color(0,0,0));  // ground  color
       page.fillRect(0,380,600,20);      // ground
 
       int index = 0;
       index += (int)(Math.random()*10+15);
-      while (index <= 500){
+      while (index <= 500){                         //draws randomly-sized buildings and randomly-sized gaps
         int leWidth = (int)(Math.random()*30+60);
         building1 = new Building(index, 380, new Color(0,0,0), leWidth, (int)(Math.random()*100+150));
         building1.draw(page);
@@ -67,6 +58,6 @@ public class SkylinePanel extends JPanel
       }
       
       page.setColor (Color.black);  // string color
-      page.drawString("David Ye Luo and Kenta Medina's Skyline with windows", 30, 30); // draw string
+      page.drawString("Welcome to David Ye Luo and Kenta Medina's City, shown here in an artist's depiction", 30, 30); // draw string
    }
 }
