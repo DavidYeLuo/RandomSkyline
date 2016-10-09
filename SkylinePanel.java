67ff23a4e0b6942c71de969f578b5d3a90a233a3
DavidYeLuo <dayeluo@galileoweb.org>
2016-10-08 21:59:36 -0700

    Improved readability

--- SkylinePanel.java
+++ SkylinePanel.java
@@ -2,7 +2,7 @@
  * SkylinePanel --- program to draw buildings
  * @author          David Ye Luo, Kenta Medina
  * @version         1.0
- * @since           2016-10-06
+ * @since           2016-09-27 
 */
 
 import javax.swing.*;
@@ -11,9 +11,8 @@ import java.lang.*;
 
 public class SkylinePanel extends JPanel
 {
-    private Building building1; //Declare
+    private Building building1; //instantiate
     private Star shinyThing;
-    
    //-------------------------------------------------
    //   Default Constructor: Sets up the Panel
    //-------------------------------------------------
@@ -22,7 +21,6 @@ public class SkylinePanel extends JPanel
       setPreferredSize (new Dimension(600, 400));
       setBackground (new Color(255,255,255));
    }
-   
    //-------------------------------------------------
    //   Draws the Panel
    //-------------------------------------------------
@@ -30,15 +28,15 @@ public class SkylinePanel extends JPanel
    {
 
       super.paintComponent(page);
-      for(int i = 0; i < 100; i++){ // 100 iterations
-        shinyThing = new Star((int)(Math.random()*600) // random x from 0 inclusive to 600 exclusive 
-                            , (int)(Math.random()*380) // random y from 0 inclusive to 380 exclusive
-                            , Color.black, 5, 5);      // color and size
+      for(int i = 0; i < 100; i++){
+        shinyThing = new Star((int)(Math.random()*600)
+                            , (int)(Math.random()*380)
+                            , Color.black, 5, 5);
         shinyThing.draw(page);
       }
 
-      page.setColor(Color.black);   // moon
-      page.fillOval(50,70,40,40);   // moon
+      page.setColor(Color.black);  // moon
+      page.fillOval(50,70,40,40);  // moon
       
       page.setColor(new Color(0,0,0));   // far background building
       for(int i = 0; i < 40; i++){
@@ -48,17 +46,24 @@ public class SkylinePanel extends JPanel
                       ,40);                         // far background building
       }
       
-      page.setColor(new Color(0,0,0));  // ground color
+      page.setColor(new Color(0,0,0));  // ground  color
       page.fillRect(0,380,600,20);      // ground
 
-      int index = 0;
-      index += (int)(Math.random()*10+15);
-      while (index <= 500){                         //draws randomly-sized buildings and randomly-sized gaps
-        int leWidth = (int)(Math.random()*30+60);
-        building1 = new Building(index, 380, new Color(0,0,0), leWidth, (int)(Math.random()*100+150));
+      int index = 0; // initialized
+      index += (int)(Math.random()*10+15); // changed from 0 to random 34
+      while (index <= 500){                         // draws randomly-sized buildings and randomly-sized gaps
+      
+        int leWidth = (int)(Math.random()*30+60);   // Building's width
+        
+        building1 = new Building(index, 380         // Building's Location
+                                , new Color(0,0,0)  // Black color
+                                , leWidth
+                                , (int)(Math.random()*100+150));  // Building's height
+                                
         building1.draw(page);
-        index += leWidth;
-        index += (int)(Math.random()*20+20);
+        
+        index += leWidth;   // update index
+        index += (int)(Math.random()*20+20); // ???
       }
       
       page.setColor (Color.black);  // string color
